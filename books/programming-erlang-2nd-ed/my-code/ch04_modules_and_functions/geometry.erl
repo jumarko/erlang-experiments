f3d93896565cc93d5cd3b32bfc4de4b88b4639d1
Juraj Martinka <jumarko@gmail.com>
2025-12-01 10:31:40 +0100

    Programming Erlang - Ch4: adding geometry.erl.

---
+++ books/programming-erlang-2nd-ed/my-code/ch04_modules_and_functions/geometry.erl
@@ -0,0 +1,15 @@
+%% Basic geometry module defined on p. 44, Ch4.
+-module(geometry).
+-export([area/1, test/0]).
+
+area({rectangle, Width, Height}) -> Width * Height;
+area({square, Side}) -> Side * Side.
+
+%% compile the module and call geometry:test().
+%% Example error: 
+%%    ** exception error: no match of right hand side value 144
+%%         in function  geometry:test/0 (.../ch04_modules_and_functions/geometry.erl:12)
+test() ->
+    12 = area({rectangle, 3, 4}),
+    144 = area({square, 12}),
+    tests_worked.
